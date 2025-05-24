2ceaaa39ac8e474b11b663169dfff3cea7c6c30a
magicgamer456 <aaronfernan78@gmail.com>
2025-05-24 21:24:39 +0530

    - Added some comments to server.js

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+//Imports required
 const express = require('express');
 const path = require('path');
 
+// initializes the app
 const app = express();
 
+//The server starts from here
 console.log("\x1b[1m\x1b[33mStarting\x1b[0m\x1b[90m | \x1b[0mStarting Server...");
 
+//Set up view engine for express
 app.set("view engine", "ejs");
 
+//Route setup
 app.get("/", (req, res) => {
   res.redirect('/home');
 });
@@ -26,12 +31,15 @@ app.get("/cources", (req, res) => {
   res.render('cources');
 });
 
+// Router Setup
 const toolsRouter = require("./routers/tools.js");
 app.use("/tools", toolsRouter) ;
 
+//Setup static directorys
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'assets')));
 
+//Server is now online
 const PORT = "8000";
 app.listen(PORT, () => {
   console.log(`\x1b[1m\x1b[32mSuccess\x1b[0m\x1b[90m | \x1b[0mServer online on http://localhost:${PORT}.`);
