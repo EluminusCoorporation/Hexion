0eb56a4558a4c0c1accae37cfb43e670b311b8e8
magicgamer456 <aaronfernan78@gmail.com>
2025-05-26 16:53:14 +0530

    - Added more comments - Rearranged the events accordingly

--- server.js
+++ server.js
@@ -48,23 +48,7 @@ app.use((req, res, next) => {
   //separate middleware for 404
   res.status(404).render('error_handling/errorbody', {error: 'Could not find page', errorurl: req.url, errorcode: '404'})
 })
-app.use((err, req, res, next) => {
-  //Logs the error stack for debugging purposes
-  console.log(err.stack);
-  
-  const status = err.status || 500;
-  
-  const messages = {
-    400: 'Bad Request', 
-    401: 'Unauthorized Access', 
-    402: 'Forbidden', 
-    404: 'Could not find page', 
-    500: 'Internal server error'
-  };
-  const message = messages[status];
-  
-  res.status(status).render('error_handling/errorbody', { error: message, errorcode: status, errorurl: req.url});
-});
+
 // Router Setup
 const toolsRouter = require("./routers/tools.js");
 app.use("/tools", toolsRouter) ;
@@ -73,6 +57,28 @@ app.use("/tools", toolsRouter) ;
 app.post('/upload', async (req, res) => {
   
 })
+ 
+app.use((err, req, res, next) => {
+  //Logs the error stack for debugging purposes
+  console.log(err.stack);
+  
+  //Gets the status code
+  const status = err.status || 500;
+  
+  //Sets error codes for certain status codes
+  const messages = {
+    400: 'Bad Request', 
+    401: 'Unauthorized Access', 
+    402: 'Forbidden', 
+    404: 'Could not find page', 
+    500: 'Internal server error'
+  };
+  //Gets the correct message for the status code
+  const message = messages[status];
+  
+  //Sends data to the frontend
+  res.status(status).render('error_handling/errorbody', { error: message, errorcode: status, errorurl: req.url});
+});
 
 //Server is now online
 const PORT = "8000";
