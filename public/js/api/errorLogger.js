b88d1cf6c2bcb6e3e8012115a39a4c04e1f98026
magicdevm <aaronfernan78@gmail.com>
2025-06-10 20:36:37 +0530

    Added comments to all api files

--- public/js/api/errorLogger.js
+++ public/js/api/errorLogger.js
@@ -1,12 +1,18 @@
 import { selectedExt } from './sliderbtn.js'
-
+//Sets an error timeout var
 let errorTimeout;
+//Global setError func
 export function setError(message) {
+  //Clears old timeouts before execution
   clearTimeout(errorTimeout);
+  //Gets required imports
   var errorText = document.getElementById('errorText');
   var errorDiv = document.getElementById('errorDiv');
+  //Enables the Error screen
   errorDiv.style.display = "flex";
+  //Adds error to the error screen
   errorText.textContent = message;
+  //If text content is nothing it'll just close the error screen
   if (errorText.textContent === "") {
     errorDiv.style.display = "none"
   }
@@ -18,12 +24,18 @@ export function setError(message) {
   }, 7000);
 };
 
+//Used for SetError In file menu Func
 export function setErrorFile(message) {
+  //Clears old timeouts before execution
   clearTimeout(errorTimeout);
+  //Gets required imports
   var errorText = document.getElementById('errorTextUPLOAD');
   var errorDiv = document.getElementById('errorDivUPLOAD');
+  //Enables error screen
   errorDiv.style.display = "flex";
+  //Adds error to the error screen
   errorText.textContent = message;
+  //If text content is nothing it'll just close the error screen
   if (errorText.textContent === "") {
     errorDiv.style.display = "none"
   }
@@ -35,65 +47,90 @@ export function setErrorFile(message) {
   }, 7000);
 } 
 
+//Global error logger func used before the Execution of a FUNC
 export function errorLoggerBEFORE(name, text) {
+  //Checks if name is not provided
   if (name === "") {
     setError('Please select a language')
     return false;
   }
+  
+  //Checks if text is not provided
   if (text === "") {
     setError('Text cannot be empty!');
     return false;
   }
+  //Else clears the timeout and goes forward
   setError('')
   return true;
 }
 
+//Global error logger func used after the execution of a FUNC
 export function errorLoggerAFTER(text) {
+  //Checks if text has characters that are not valid
   if (text.includes("�")) {
     resultsDiv.style.display = "none";
     setError('Something went wrong, did you enter a valid text ?');
     return false;
   }
+  //var used to check if there are invalid characters in a text
   const hasInvalidChars = /[\x00-\x1F]/.test(text);
+  //Checks if text has invalid characters(Advance way)
   if (hasInvalidChars) {
     resultsDiv.style.display = "none";
     setError('Something went wrong, did you enter a valid text ?');
     return false;
   }
+  //Checks if text is empty
   if (text === "") {
     resultsDiv.style.display = "none";
     setError('Something went wrong, did you enter a valid text ?');
     return false;
   }
+  //Else goes forward
   return true;
 };
 
+//Global file logger func
 export function fileLogger(files) {
+  //Gets the first file
   const file = files[0]
+  //Checks if file exists
   if (!file) return false;
+  //File name
   const fileName = file.name;
+  //File size
   const fileSize = file.size;
+  //File extension
   const fileExtension = fileName.split('.').pop().toLowerCase();
+  //Extensions supported
   const supportedExtensions = ["py", "js", "html", "css"]
+  //Checks if file exists (2nd way)
   if (files.length === 0) {
     setError('Something went wrong did you select a file?');
     return false;
   };
+  //Sets selectedExt as auto
   if (selectedExt === "auto") {
+    //Checks if Extension is supported
     if (!supportedExtensions.includes(fileExtension)) {
       setErrorFile('File Extension not supported by our service');
       return false;
     }
+    //Else continues
     setErrorFile('')
     return true;
   }
+  //Else checks if not supported
   else if (fileExtension !== selectedExt) {
     setErrorFile(`File Extension not supported by the language you have selected (.${selectedExt})`);
     return false;
   }
+  //Checks if file size exceeds the file size, limit
   if (file.size > 15728670) {
     setErrorFile('File is too large');
     return false;
   }
+  //Else goes forward
   return true;
 }
--- public/js/api/sliderbtn.js
+++ public/js/api/sliderbtn.js
@@ -1,43 +1,61 @@
+//Gets required imports
 var dropDownMenu = document.getElementById("dropDownMenu");
 var dropDownCon = document.getElementById('dropDownCon');
 var dropDownIcon = document.getElementById('ddIcon');
 var auto = document.getElementById('auto');
 var selectItem = document.getElementsByClassName('select-item');
-export var selectedExt = "auto"
+//Gets selected Ext(only for errordebugger)
+export var selectedExt = "auto";
 
-auto.classList.add('selected')
+//Enables auto as default (only for errordebugger)
+auto.classList.add('selected');
+
+//event listener for click
 dropDownMenu.addEventListener('click', function() {
+  //Enables Stylers
   dropDownContent.classList.toggle("active");
   dropDownIcon.classList.toggle("active");
+  //Sets the previous selected to auto(only for error debugger)
   let previousSelected = auto;
+  
+  //Makes a event listener for all Items
   var i;
   for (i = 0; i < selectItem.length; i++) {
     selectItem[i].addEventListener('click', function() {
       var itemSelect = document.getElementById('dropdown-text');
       const span = document.getElementById('spanR');
+      //Sets Selected Ext
       selectedExt = this.dataset.ext;
       name = this.innerHTML;
+      //Removes recommended tag
       if (span) {
         let removeSpanTC = this.cloneNode(true);
         removeSpanTC.querySelector('#spanR')?.remove();
         name = removeSpanTC.innerHTML.trim()
       }
+      //Ignores if name is Same as the selected one
       if (name === itemSelect.textContent) return;
+      //Else starts the process
       else {
         itemSelect.style.color = "black"
         itemSelect.innerHTML = name;
       }
+      //Sets previous selected to Not selected
       if (previousSelected) {
         previousSelected.classList.remove('selected')
       }
-      this.classList.add('selected')
-      previousSelected = this
+      //adds selected
+      this.classList.add('selected');
+      //sets previously selected again
+      previousSelected = this;
       return;
     });
   }
+  //Closes the drop down menu
   if (dropDownMenu.style.maxHeight === "500px") {
     dropDownMenu.style.maxHeight = "14px";
   }
+  //Opens the drop down menu
   else {
     dropDownMenu.style.maxHeight = "500px"
   }
