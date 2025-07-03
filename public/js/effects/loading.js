a22c239e8e3869b4cc95583b4c9540ff26d1168e
magicdevm <aaronfernan78@gmail.com>
2025-07-03 12:16:14 +0530

    Minor comment changes

--- public/js/effects/loading.js
+++ public/js/effects/loading.js
@@ -1,5 +1,5 @@
-// Dependency for incoming.js
 // Loads the Loading screen when the user loads the page
 window.onload = function() {
+  //adds the required classList
   document.getElementById("preloader").classList.add("fade-out");
 };
