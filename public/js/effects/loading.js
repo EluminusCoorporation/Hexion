a5bf61441e82ec66c584c00af94f9c89626f3fe6
magicdevm <aaronfernan78@gmail.com>
2025-07-04 16:07:32 +0530

    Changed the element of preloader to loader for a better viewing perspective

--- public/js/effects/loading.js
+++ public/js/effects/loading.js
@@ -1,5 +1,5 @@
 // Loads the Loading screen when the user loads the page
 window.onload = function() {
   //adds the required classList
-  document.getElementById("preloader").classList.add("fade-out");
+  document.getElementById("loader").classList.add("fade-out");
 };
