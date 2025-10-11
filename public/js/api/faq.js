b5bbcf82b290a5dfb403c4d0b0ce21d8cbd3800c
magicdevm <aaronfernan78@gmail.com>
2025-10-11 23:16:00 +0530

    feat[home]: better faq system

--- public/js/api/faq.js
+++ public/js/api/faq.js
@@ -1,20 +1,12 @@
 // Necessary element import
-var faq = document.getElementsByClassName("faq-box-question");
+var faq = document.querySelectorAll(".faq-box-question");
 
-// i is a placeholder for the clicked faq
-var i;
-// finds out which faq was clicked and assigned to i
-for (i = 0; i < faq.length; i++) {
-  faq[i].addEventListener('click', function() {
+// Makes it usable
+faq.forEach((faqs, i) => {
+  faqs.addEventListener('click', function() {
     // Activates the faq
     this.classList.toggle("active");
-    var body = this.nextElementSibling;
-    if (body.style.maxHeight === "500px") {
-      body.style.maxHeight = "0px";
-    }
-    // Deactivates the faq
-    else {
-      body.style.maxHeight = "500px"
-    }
+    var ans = this.nextElementSibling;
+    ans.classList.toggle("active");
   });
-};
+})
