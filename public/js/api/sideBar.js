866ab045c3888bc71bf685f84dbc456a80cda17a
magicdevm <aaronfernan78@gmail.com>
2025-10-15 23:23:50 +0530

    feat[header]: close on redirect sys

--- public/js/api/sideBar.js
+++ public/js/api/sideBar.js
@@ -1,6 +1,7 @@
 var sideBarButton = document.getElementById('sideBarButton');
 const sideBar = document.getElementById('sideBar');
-const backgroundFilter = document.getElementById('backgroundSideBar')
+const backgroundFilter = document.getElementById('backgroundSideBar');
+const links = document.querySelectorAll('.nav-bar .side-bar ul li');
 
 sideBarButton.addEventListener('click', () => {
   backgroundFilter.classList.toggle('open');
@@ -10,4 +11,11 @@ sideBarButton.addEventListener('click', () => {
 backgroundFilter.addEventListener('click', () => {
   sideBar.classList.remove('open');
   backgroundFilter.classList.remove('open');
+});
+
+links.forEach((link, i) => {
+  link.addEventListener('click', () => {
+    sideBar.classList.remove('open');
+    backgroundFilter.classList.remove('open');
+  });
 });
