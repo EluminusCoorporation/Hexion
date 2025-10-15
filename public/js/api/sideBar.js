88b9e63ef746ec0f4d0191c8ef2e214373646706
magicdevm <aaronfernan78@gmail.com>
2025-10-15 23:14:15 +0530

    feat[header]: added blur background on sidebar

--- public/js/api/sideBar.js
+++ public/js/api/sideBar.js
@@ -1,6 +1,8 @@
 var sideBarButton = document.getElementById('sideBarButton');
 const sideBar = document.getElementById('sideBar');
+const backgroundFilter = document.getElementById('backgroundSideBar')
 
 sideBarButton.addEventListener('click', () => {
+  backgroundFilter.classList.toggle('open');
   sideBar.classList.toggle('open');
 });
