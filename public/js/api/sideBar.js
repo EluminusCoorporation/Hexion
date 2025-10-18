ffbbdb13ae5417616a4c904acad50406eb55d338
magicdevm <aaronfernan78@gmail.com>
2025-10-18 17:23:47 +0530

    feat[header]: Added Sub Menu closing on sidebar close & redirects

--- public/js/api/sideBar.js
+++ public/js/api/sideBar.js
@@ -2,12 +2,22 @@
 var sideBarButton = document.getElementById('sideBarButton');
 const sideBar = document.getElementById('sideBar');
 const backgroundFilter = document.getElementById('backgroundSideBar');
+
+const subMenu = document.querySelectorAll('.sub-menu');
 const links = document.querySelectorAll('.nav-bar .side-bar ul li a');
 
 // Makes an event listener for sidebar opening & closing
 sideBarButton.addEventListener('click', () => {
   backgroundFilter.classList.toggle('open');
   sideBar.classList.toggle('open');
+  
+  // Closes the opened sub Menus
+  subMenu.forEach((subMenus) => {
+    const subMenuButton = subMenus.parentNode
+    const subMenuToggler = subMenuButton.querySelector('.toggle-button');
+    subMenuToggler.classList.remove('open');
+    subMenus.classList.remove('open');
+  });
 });
 
 // Event listener for closing on outside click
@@ -21,5 +31,13 @@ links.forEach((link, i) => {
   link.addEventListener('click', () => {
     sideBar.classList.remove('open');
     backgroundFilter.classList.remove('open');
+    
+    // Closes the opened sub Menus
+    subMenu.forEach((subMenus) => {
+      const subMenuButton = subMenus.parentNode
+      const subMenuToggler = subMenuButton.querySelector('.toggle-button');
+      subMenuToggler.classList.remove('open');
+      subMenus.classList.remove('open');
+    });
   });
 });
