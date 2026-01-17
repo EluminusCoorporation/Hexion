60db791d6277b80d1f1b2fbac9c3c656910ade33
magicdevm <aaronfernan78@gmail.com>
2026-01-17 17:40:39 +0530

    feat(request): refactored the request function

--- public/js/tools/request.js
+++ public/js/tools/request.js
@@ -4,24 +4,24 @@ import { setFunction } from "../api/dropDownMenu.js";
 import {} from "../api/copy.js";
 
 function refreshBody() {
-  const requestBodyType = document.querySelector('.body-type');
-  const bodyField = document.getElementById('contentBody');
-  
+  const requestBodyType = document.querySelector(".body-type");
+  const bodyField = document.getElementById("contentBody");
+
   switch (requestBodyType.textContent) {
-    case 'application/json':
-      bodyField.placeholder = '{ "key": "value" }'
+    case "application/json":
+      bodyField.placeholder = '{ "key": "value" }';
       break;
-    case 'application/x-www-form-urlencoded':
-      bodyField.placeholder = 'key=value'
+    case "application/x-www-form-urlencoded":
+      bodyField.placeholder = "key=value";
       break;
-    case 'application/xml':
-      bodyField.placeholder = '<data></data>'
+    case "application/xml":
+      bodyField.placeholder = "<data></data>";
       break;
-    case 'text/plain':
-      bodyField.placeholder = 'your data here!'
+    case "text/plain":
+      bodyField.placeholder = "your data here!";
       break;
     default:
-      bodyField.placeholder = 'body'
+      bodyField.placeholder = "body";
       break;
   }
 }
@@ -30,113 +30,112 @@ setFunction(refreshBody);
 
 const resultsBtn = document.getElementById("results-btn");
 
-document.querySelectorAll('.toggle').forEach((el) => {
+document.querySelectorAll(".toggle").forEach(el => {
   el.addEventListener("change", () => {
     const elementToEnable = document.getElementById(el.dataset.el);
-    
+
     if (!elementToEnable) {
-      console.error('Element to enable does not exists')
+      console.error("Element to enable does not exists");
+    }
+
+    elementToEnable.style.display = el.checked ? "flex" : "none";
+  });
+});
+
+// Makes an event listener for results button
+resultsBtn.addEventListener("click", async () => {
+  try {
+    // toggle the loader on
+    toggleLoader(true);
+    
+    let body;
+    const url = document.getElementById("ttc").value;
+
+    const type = document.querySelector(".type").textContent.toUpperCase();
+    const content = document
+      .querySelector(".content")
+      .textContent.toLowerCase();
+    const accept = document.querySelector(".accept").textContent.toLowerCase();
+    const bodyValue = document.getElementById("contentBody").value;
+
+    const auth = document.querySelector(".authToggler");
+    const authField = document.getElementById("auth");
+    
+    // check if all values are present
+    if (type === "GET") {
+      if (!url) {
+        throw new Error("You've not filled in all the fields.");
+      }
+    } else {
+      if (!url || !content) {
+        throw new Error("You've not filled in all the fields.");
+      }
     }
     
-    elementToEnable.style.display = el.checked ? 'flex' : 'none';
-  });
-})
-
-//Makes an event listener for results button
-resultsBtn.addEventListener("click", function () {
-  toggleLoader(true);
-  const url = document.getElementById("ttc").value;
-
-  const type = document.querySelector(".type").textContent.toUpperCase();
-  const content = document.querySelector(".content").textContent.toLowerCase();
-  const accept = document.querySelector(".accept").textContent.toLowerCase();
-
-  const bodyValue = document.getElementById("contentBody").value;
-  const body = JSON.parse(bodyValue);
-  
-  const auth = document.querySelector('.authToggler');
-  const authField = document.getElementById('auth');
-  
-  /* if (!url.startsWith('https')) {
-    setStatus('error', 'Request sender failed', 'Cannot request internal api')
-    toggleLoader(false);
-    return;
-  } */
-  
-  if (url.startsWith('http://localhost')) {
-    setStatus('error', 'Request Sender failed', 'Cannot request LocalHosts')
-    toggleLoader(false);
-    return;
-  }
-  
-  if (type === "GET") {
-    if (!url) {
-      setStatus(
-        "error",
-        "Fields Required",
-        "You've not filled in all the fields"
-      );
-      toggleLoader(false);
-      return;
+    // check if its trying to ping internal api
+    /* if (!url.startsWith('https')) {
+    throw new Error("Cannot request internal api.")
+    } */
+    
+    // check if its trying to ping localhosts
+    if (url.startsWith("http://localhost")) {
+      throw new Error("Cannot request Localhosts.");
     }
-  } else {
-    if (!url || !content) {
-      setStatus(
-        "error",
-        "Fields Required",
-        "You've not filled in all the fields"
-      );
-      toggleLoader(false);
-      return;
+    
+    // check if the given body is an usable object
+    if (bodyValue) {
+      body = JSON.parse(bodyValue);
+      
+      if (typeof body !== "object") {
+        throw new Error("The given body is invalid");
+      }
     }
-  }
-  
-  if (typeof body !== "object") {
-    setStatus('error', 'Request Sender failed', 'The given body is invalid')
+
+    const fetchObject = {
+      method: type,
+      headers: {
+        "Content-Type": content,
+        // only send accept if user has provided one
+        ...(accept && { Accepts: accept }),
+        // only send auth if user has checked it and provided one
+        ...(auth.checked && authField && { Authorization: `Bearer ${authField.value}` })
+      },
+      // only send body if user has provided one
+      ...(body && { body: JSON.stringify({ ...body })}),
+    };
+    
+    // remove certain attributes if its an GET request
+    if (type === "GET") {
+      delete fetchObject.body;
+      delete fetchObject.headers;
+    }
+    
+    // Display the sent headers for debugging purposes
+    document.getElementById("headerContainer").style.display = "flex";
+    document.getElementById("headers").textContent = JSON.stringify(fetchObject);
+      
+    // make the actual fetch request
+    const response = await fetch(url, fetchObject);
+    // throw error if request fails
+    if (!response.ok) throw new Error(`${response.status}`);
+    
+    const data = await response.json();
+    // throw error if data has error attribute
+    if (data.error) throw new Error(data.error);
+    
+    // set the sent data for display
+    const results = document.getElementById("results");
+    results.textContent = JSON.stringify(data);
+    
+    // display the data
+    const resultsDiv = document.getElementById("resultsDiv");
+    resultsDiv.style.display = "flex";
+  } catch (error) {
+    // catch every error and display
+    setStatus("error", "Request Sender Failed", error);
+    console.error("An error occured while requesting api " + error)
+  } finally {
+    // disable the loader at the end no matter whats the result
     toggleLoader(false);
-    return
   }
-  
-  const fetchObject = {
-    method: type,
-    headers: { 
-      "Content-Type": content, 
-      ...(accept && { Accepts: accept }), 
-      ...(auth.checked && { Authorization: `Bearer ${authField.value}` })
-    },
-    body: JSON.stringify({ ...body })
-  };
-
-  if (type === "GET") {
-    delete fetchObject.body;
-    delete fetchObject.headers;
-  }
-  
-  document.getElementById('headerContainer').style.display = "flex";
-  document.getElementById('headers').textContent = JSON.stringify(fetchObject);
-  
-  fetch(url, fetchObject)
-    .then(response => {
-      if (!response.ok) throw new Error(`${response.status}`);
-      return response.json();
-    })
-    .then(data => {
-      if (data.error) throw new Error(data.error);
-
-      const results = document.getElementById("results");
-      results.textContent = JSON.stringify(data);
-      const resultsDiv = document.getElementById("resultsDiv");
-      resultsDiv.style.display = "flex";
-    })
-    .catch(error => {
-      console.log("Error sending request:", error);
-      setStatus(
-        "error",
-        "Request Sender failed",
-        "An error occured on sending request: " + error
-      );
-    })
-    .finally(() => {
-      toggleLoader(false);
-    });
 });
