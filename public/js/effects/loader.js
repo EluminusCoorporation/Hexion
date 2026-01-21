6439e600384bbacecc48e5aeb283d9bfb471e9ad
magicdevm <aaronfernan78@gmail.com>
2026-01-21 16:50:30 +0530

    feat(ui): better headers & footer handling

--- public/js/effects/loader.js
+++
@@ -1,9 +0,0 @@
-// Loads the Loading screen when the user loads the page
-function toggleLoader(value) {
-  if (value === true) document.getElementById("loader").classList.remove("fade-out");
-  if (value === false) document.getElementById("loader").classList.add("fade-out");
-}
-window.onload = function() {
-  //adds the required classList
-  toggleLoader(false)
-};
