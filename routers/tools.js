9e7d9de1f3a9a5b0d923de24d5e30981eb036476
magicdevm <aaronfernan78@gmail.com>
2026-02-10 17:22:38 +0530

    fix(errordebugger): missing icon library import

--- routers/tools.js
+++ routers/tools.js
@@ -54,6 +54,7 @@ router.get("/errorDebugger", (req, res) => {
   const pageInfo = {
     title: "Error Debugger",
     description: "The Error Debugger, tool of hexion, here you can fix errors in you're code by simply typing the erroring code block or uploading the whole file, you get chance to select from 10 different languages",
+    extraLinks: "<script src='https://kit.fontawesome.com/57901f661d.js' crossorigin='anonymous' defer></script>",
     url: `${config.general.domain}/tools/errorDebugger`,
     path: "tools/errorDebugger"
   };
@@ -74,7 +75,6 @@ router.get("/request", (req, res) => {
   const pageInfo = {
     title: "Request Sender",
     description: "The request sender tool of hexion, with this tool you can send an request to any of you're apis for testing purposes we support all types POST, GET, PUT and DELETE with the option to provide different types of inputs",
-    extraLinks: "<script src='https://kit.fontawesome.com/57901f661d.js' crossorigin='anonymous' defer></script>",
     url: `${config.general.domain}/tools/request`,
     path: "tools/request"
   };
