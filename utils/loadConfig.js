20db869362577da4172a5fa1b93f67fc2250f08e
magicdevm <aaronfernan78@gmail.com>
2026-02-12 21:31:42 +0530

    feat(app): improved loadConfig()

--- utils/loadConfig.js
+++ utils/loadConfig.js
@@ -1,23 +1,22 @@
 const fs = require('fs');
 const toml = require('@iarna/toml');
 
-//Sets an placeholder config
+// Sets an placeholder config
 let config = null;
 
 function loadConfig(path= 'config.toml') {
   try {
-    //Read the toml file
+    // Read the toml file
     const tomlRaw = fs.readFileSync(path, 'utf-8');
     
-    //Parse the toml to get an usable copy
+    // Parse the toml to get an usable copy
     config = toml.parse(tomlRaw);
     
-    //returns the config
+    // returns the config
     return config;
   } catch (err) {
-    //Throw error if error occured
+    // Throw error if error occured
     console.error(chalk.white(chalk.bold.blue('[config]') + ' An error occured, while reading or parsing the config.\n\n'), chalk.gray(err));
-    throw err;
   };
 };
 
