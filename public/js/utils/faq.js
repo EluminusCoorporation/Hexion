6eac3f4a7410ce8c344576ed79a1d59213e2c4fc
magicdevm <aaronfernan78@gmail.com>
2026-02-22 22:41:50 +0530

    feat(ui): better responsiveness

--- public/js/utils/faq.js
+++ public/js/utils/faq.js
@@ -1,13 +1,14 @@
 // Necessary element import
 const faq = document.querySelectorAll(".faq-box-question");
-if (!faq) return;
 
 // Makes it usable
-faq.forEach((faqs, i) => {
-  faqs.addEventListener('click', function() {
-    // Activates the faq
-    this.classList.toggle("active");
-    const ans = this.nextElementSibling;
-    ans.classList.toggle("active");
-  });
-})
+if (faq) {
+  faq.forEach((faqs, i) => {
+    faqs.addEventListener('click', function() {
+      // Activates the faq
+      this.classList.toggle("active");
+      const ans = this.nextElementSibling;
+      ans.classList.toggle("active");
+    });
+  })
+}
