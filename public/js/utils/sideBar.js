f8bc7eb197e26e373315daa9e77ee2f34086a465
magicdevm <aaronfernan78@gmail.com>
2026-02-25 22:16:56 +0530

    fix(ui): illegal return statement in sidebar

--- public/js/utils/sideBar.js
+++ public/js/utils/sideBar.js
@@ -3,8 +3,6 @@ const sideBarButton = document.getElementById("sideBarButton");
 const sideBar = document.getElementById("sideBar");
 const backgroundFilter = document.getElementById("backgroundSideBar");
 
-if (!sideBar) return;
-
 const subMenu = document.querySelectorAll(".sub-menu");
 const links = document.querySelectorAll(".nav-bar .side-bar ul li a");
 
