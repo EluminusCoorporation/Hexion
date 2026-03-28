fad7a1586fe0e0a4eafa9c49ef20f80ae2bf23f6
magicdevm <aaronfernan78@gmail.com>
2026-03-28 18:09:19 +0530

    fix(ui:color-converter): leakage of elements

--- public/js/tools/color.js
+++ public/js/tools/color.js
@@ -1,8 +1,8 @@
-//Imports the required functions
+// Imports the required functions
 import { setStatus } from "../handlers/errorLogger.js";
 import {} from "../handlers/dropDownMenu.js";
 import {} from "../handlers/copy.js";
 
 const resultsBtn = document.getElementById("results-btn");
-//Makes an event listener for results button
+// Makes an event listener for results button
 resultsBtn.addEventListener("click", function () {});
