7373820a9db78a44c0efce3af5718c0b82b17475
magicdevm <aaronfernan78@gmail.com>
2026-03-30 13:37:28 +0530

    feat(droopdowns): refined css & js

--- public/js/handlers/dropDownMenu.js
+++ public/js/handlers/dropDownMenu.js
@@ -1,5 +1,5 @@
 // Gets required imports
-const dropDownMenu = document.querySelectorAll("#dropDownMenu")
+const dropDownMenu = document.querySelectorAll("#dropDownMenu");
 
 let actionRegistry = {};
 export function setFunction(func) {
@@ -9,41 +9,49 @@ export function setFunction(func) {
 if (dropDownMenu) {
   // make an listener for each dropdown
   dropDownMenu.forEach(el => {
+    const dropDownContent = el.querySelector("#dropDownContent");
+    
     el.addEventListener("click", function () {
-      const dropDownContent = el.querySelector("#dropDownContent");
-      const dropDownIcon = el.querySelector("#dropdownIcon");
-  
-      // Enables Stylers
+      // Toggle the dropdown
       dropDownContent.classList.toggle("show");
-      dropDownIcon.classList.toggle("active");
-  
-      // Makes a event listener for all Items
-      dropDownContent.addEventListener("click", event => {
-        if (event.target.classList.contains("select-item")) {
-          const itemSelect = el.querySelector("#dropdownSelected");
-          const name = event.target.innerHTML;
-          // Ignores if name is Same as the selected one
-          if (name === itemSelect.dataset.selected) return;
-          // Else starts the process
-          else {
-            itemSelect.style.color = "var(--text)";
-            itemSelect.innerHTML = name;
-            itemSelect.dataset.selected = event.target.textContent;
-          }
-          // Unselects the previously selected item
-          const selectedItems = dropDownContent.querySelectorAll(".selected");
-          selectedItems.forEach(item => item.classList.remove("selected"));
-  
-          // adds selected
-          event.target.classList.add("selected");
-  
-          const actionName = el.dataset.action;
-          const action = actionRegistry[actionName];
-  
-          // silent runner
-          if (typeof action === "function") action();
-        }
-      });
+      // Toggle the icon
+      el.querySelector("#dropdownIcon").classList.toggle("active");
+    });
+    
+    // Makes a event listener for all Items
+    dropDownContent.addEventListener("click", event => {
+      if (!event.target.classList.contains("select-item")) return;
+      let name = event.target.innerHTML;
+      
+      // If even a single tag is applied, remove it
+      const tags = event.target.querySelector('.tag');
+      if (tags) {
+        // Clone it to avoid removing actual tags
+        const itemClone = event.target.cloneNode(true);
+        // Remove all tags
+        itemClone.querySelectorAll('.tag').forEach(tag => tag.remove());
+        // Apply the sanitized name
+        name = itemClone.innerHTML;
+      };
+      
+      // Ignores if name is Same as the selected one
+      const selectorItem = el.querySelector("#dropdownSelected");
+      if (event.target.textContent !== selectorItem.dataset.selected) {
+        selectorItem.style.color = "var(--text)";
+        selectorItem.innerHTML = name;
+        selectorItem.dataset.selected = event.target.textContent;
+      };
+      
+      // Unselects the previously selected item(s)
+      dropDownContent.querySelectorAll(".selected").forEach(item => item.classList.remove("selected"));
+
+      // adds selected
+      event.target.classList.add("selected");
+
+      const actionName = el.dataset.action;
+      const action = actionRegistry[actionName];
+      // silent runner
+      if (typeof action === "function") action();
     });
   });
 }
