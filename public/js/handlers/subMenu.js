9431866714b5c57111680df7113e2e338e9708f0
MagicDevM <aaronfernan78@gmail.com>
2026-09-25 20:48:25 +0530

    fix(submenu): the icon not rotating

--- public/js/handlers/subMenu.js
+++ public/js/handlers/subMenu.js
@@ -1,11 +1,11 @@
-const subMenuButton = document.querySelectorAll('.sub-menu-toggler');
+const subMenuButton = document.querySelectorAll(".sub-menu-toggler");
 
 // Event listener sub menus
 subMenuButton.forEach((subMenuButtons) => {
-  subMenuButtons.addEventListener('click', function() {
+  subMenuButtons.addEventListener("click", function () {
     //Activates the submenu
-    const subMenu = this.parentNode.querySelector('.sub-menu');
-    this.classList.toggle('open')
-    subMenu.classList.toggle('open');
+    const subMenu = this.parentNode.querySelector(".sub-menu");
+    this.querySelector(".toggle-button").classList.toggle("open");
+    subMenu.classList.toggle("open");
   });
 });
